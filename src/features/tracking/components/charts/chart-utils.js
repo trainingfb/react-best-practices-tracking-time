4b52873a23d00d6c2c75b98160af3b480d8d7584
Fabio Biondi <info@fabiobiondi.com>
2018-09-27 02:12:42 +0200

    Fix Users icon

--- src/features/tracking/components/charts/chart-utils.js
+++ src/features/tracking/components/charts/chart-utils.js
@@ -5,7 +5,7 @@ export const options = {
 export const typeConfig = {
   'development': { color:"#F7464A",  highlight: "#FF5A5E", icon: 'fa fa-code' },
   'study': { color:"#46BFBD",  highlight: "#5AD3D1", icon: 'fa fa-book'  },
-  'meeting': { color:"#FDB45C",  highlight: "#FFC870", icon: 'fa fa-code'  },
+  'meeting': { color:"#FDB45C",  highlight: "#FFC870", icon: 'fa fa-users'  },
   'undefined': { color:"#FDB45C",  highlight: "#FFC870", icon: null},
 };
 
