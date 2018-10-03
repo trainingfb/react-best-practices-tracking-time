df6ee88477541092fd5a3d28536b5e7ada0f1d88
Gian Marco Toso <info@gianmarcotoso.com>
2018-10-03 12:01:32 +0200

    switch to render props

--- src/features/tracking/components/charts/Charts.jsx
+++ src/features/tracking/components/charts/Charts.jsx
@@ -2,28 +2,27 @@ import React from 'react'
 import { getLineData, getPieData, options } from './chart-utils'
 import { Line as LineChart } from 'react-chartjs'
 import { Pie as PieChart } from 'react-chartjs'
+import Tracking from '../../Tracking'
 
 const Charts = props => {
-	if (props.data.length === 0) {
-		return (
-			<div className="text-center">
-				<p className="text-muted">No data to display</p>
-			</div>
-		)
-	}
-
-	// generate chart data
-	const lineData = getLineData(props.data)
-	const pieData = getPieData(props.data)
-
 	return (
-		<div className="text-center">
-			<h4>Task Duration</h4>
-			<LineChart height="200" data={lineData} options={options} />
-			<hr />
-			<h4>Task Amount</h4>
-			<PieChart height="200" data={pieData} options={options} />
-		</div>
+		<Tracking>
+			{({ tasks }) =>
+				tasks.length > 0 ? (
+					<div className="text-center">
+						<h4>Task Duration</h4>
+						<LineChart height="200" data={getLineData(tasks)} options={options} />
+						<hr />
+						<h4>Task Amount</h4>
+						<PieChart height="200" data={getPieData(tasks)} options={options} />
+					</div>
+				) : (
+					<div className="text-center">
+						<div className="text-muted">No data to display</div>
+					</div>
+				)
+			}
+		</Tracking>
 	)
 }
 export default Charts
