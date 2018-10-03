6142bac30f8b26386a775305931cc23aff98c308
Gian Marco Toso <info@gianmarcotoso.com>
2018-10-03 11:44:21 +0200

    add simple store, use HoC to connect

--- src/features/tracking/TrackingContainer.jsx
+++ src/features/tracking/TrackingContainer.jsx
@@ -1,104 +1,13 @@
 import * as React from 'react'
 import TrackingView from './TrackingView'
-import { addTask, deleteTask, editTask, getAllTasks } from './services'
-
-export const INITIAL_ACTIVE_STATE = {
-	text: '',
-	creationDate: null,
-	duration: 0,
-	type: ''
-}
+import TrackingList from './TrackingList'
 
 export default class TrackingContainer extends React.Component {
-	constructor() {
-		super()
-		this.state = { active: INITIAL_ACTIVE_STATE, tasks: [], error: null }
-	}
-
-	async componentDidMount() {
-		const { error, data } = await getAllTasks()
-		this.setState({ tasks: data, error })
-	}
-
-	/**
-	 * Save Task (add / edit)
-	 * @param item
-	 */
-	saveTask(item) {
-		if (item.id) {
-			this.editTask(item)
-		} else {
-			this.addTask(item)
-		}
-	}
-
-	/**
-	 * Add new Task
-	 * @param task
-	 * @returns {Promise<void>}
-	 */
-	async addTask(task) {
-		const { error, data } = await addTask(task)
-		const tasks = [...this.state.tasks, data]
-		this.setState({ tasks, active: INITIAL_ACTIVE_STATE, error })
-	}
-
-	/**
-	 * Edit task
-	 * @param task
-	 * @returns {Promise<void>}
-	 */
-	async editTask(task) {
-		const { error, data } = await editTask(task)
-		// update collection
-		const tasks = this.state.tasks.map(el => {
-			return el.id === data.id ? data : el
-		})
-		// update state
-		this.setState({ tasks, active: data, error })
-	}
-
-	/**
-	 * Delete task
-	 * @param task
-	 * @returns {Promise<void>}
-	 */
-	async deleteTask(task) {
-		const { error } = await deleteTask(task.id)
-		// remove element from collection
-		const tasks = this.state.tasks.filter(el => task.id !== el.id)
-		// check if the deleted element was selected
-		const active = task.id === this.state.active.id ? INITIAL_ACTIVE_STATE : this.state.active
-		// update state
-		this.setState({ tasks, active, error })
-	}
-
-	/**
-	 * Set Active Task
-	 * @param item
-	 */
-	setActive(item) {
-		this.setState({ active: item })
-	}
-
-	/**
-	 * Reset Form
-	 */
-	reset() {
-		this.setState({ active: { ...INITIAL_ACTIVE_STATE } })
-	}
-
 	render() {
 		return (
 			<div>
-				<TrackingView
-					active={this.state.active}
-					tasks={this.state.tasks}
-					onTaskSave={newItem => this.saveTask(newItem)}
-					onTaskDelete={item => this.deleteTask(item)}
-					onTaskSetActive={item => this.setActive(item)}
-					onTaskReset={() => this.reset()}
-				/>
+				<TrackingView />
+				<TrackingList />
 			</div>
 		)
 	}
--- src/features/tracking/TrackingView.jsx
+++ src/features/tracking/TrackingView.jsx
@@ -1,47 +1,50 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 // feature components
-import { AddActivity } from "./components/form-activity";
-import { Activities } from "./components/activities";
-import { Charts } from "./components/charts";
+import { AddActivity } from './components/form-activity'
+import { Activities } from './components/activities'
+import { Charts } from './components/charts'
 // shared
-import { Card } from "../../shared/components";
+import { Card } from '../../shared/components'
+import WithTracking from './WithTracking'
 
 // Tracking View
-const TrackingView = props =>(<div className="row">
-  <div className="col-sm-7">
-    <Card>
-      <AddActivity {...props} />
-    </Card>
-    <hr/>
-    <Card title="Activities">
-      <Activities {...props} />
-    </Card>
-  </div>
-  <div className="col-sm-5">
-    <Charts data={props.tasks} />
-  </div>
-</div>);
+const TrackingView = props => (
+	<div className="row">
+		<div className="col-sm-7">
+			<Card>
+				<AddActivity {...props} />
+			</Card>
+			<hr />
+			<Card title="Activities">
+				<Activities {...props} />
+			</Card>
+		</div>
+		<div className="col-sm-5">
+			<Charts data={props.tasks} />
+		</div>
+	</div>
+)
 
-export default TrackingView;
+export default WithTracking(TrackingView)
 
 TrackingView.propTypes = {
-  active: PropTypes.shape({
-    id: PropTypes.number,
-    text: PropTypes.string,
-    date: PropTypes.string,
-    time: PropTypes.number
-  }),
-  tasks: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      text: PropTypes.string,
-      date: PropTypes.string,
-      time: PropTypes.number
-    }).isRequired
-  ),
-  onTaskSave: PropTypes.func.isRequired,
-  onTaskDelete: PropTypes.func.isRequired,
-  onTaskSetActive: PropTypes.func,
-  onTaskReset: PropTypes.func
-};
+	active: PropTypes.shape({
+		id: PropTypes.number,
+		text: PropTypes.string,
+		date: PropTypes.string,
+		time: PropTypes.number
+	}),
+	tasks: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number,
+			text: PropTypes.string,
+			date: PropTypes.string,
+			time: PropTypes.number
+		}).isRequired
+	),
+	onTaskSave: PropTypes.func.isRequired,
+	onTaskDelete: PropTypes.func.isRequired,
+	onTaskSetActive: PropTypes.func,
+	onTaskReset: PropTypes.func
+}
