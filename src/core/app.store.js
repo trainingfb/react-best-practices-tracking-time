2fa05ef51700a272efa947293506aff915700310
Gian Marco Toso <info@gianmarcotoso.com>
2018-10-09 14:14:56 +0200

    add redux devtools

--- src/core/app.store.js
+++ src/core/app.store.js
@@ -1,12 +1,14 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 
 import TrackingReducer from '../features/tracking/sources/tracking.reducer'
 import thunk from 'redux-thunk'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const rootReducer = combineReducers({
 	tracking: TrackingReducer
 })
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 export default store
